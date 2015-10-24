35b168be4f9ab489c44312cbe6f9b1b18fd59c35
Ryan Millay <ryanmillay@Ryans-MacBook-Pro.local>
2015-10-24 11:35:11 -0400

    first version of UserService

--- public/assignment/service/UserService.js
+++ public/assignment/service/UserService.js
@@ -1,3 +1,82 @@
 (function () {
-	
+	angular
+		.module("FormBuilderApp")
+		.factory("UserService", UserService);
+
+	function UserService() {
+		var currentUsers = [];
+
+		var service = {
+			findUserByUsernameAndPassword: findUserByUsernameAndPassword,
+			findAllUsers: findAllUsers,
+			createUser: createUser,
+			deleteUserById: deleteUserById,
+			updateUser: updateUser
+		};
+		return service;
+
+		// Accepts username and password strings.  Attempts to match to user in existing
+		// currentUsers array.  If found, executes userFoundCallback.  Else return null.
+		function findUserByUsernameAndPassword(username, password, userFoundCallback) {
+			// iterate over current users and look for a match
+			for(var i = 0, i < currentUsers.length; i++) {
+				if(currentUsers[i].username === username 
+					&& currentUsers[i].password === password) {
+					// user found!  Time to execute the callback function
+					userFoundCallback(currentUser[i]);
+				}
+				return null;
+			}
+		}
+
+		// Simply executes allUsersCallback with the array or current users
+		function findAllUsers(allUsersCallback) {
+			allUsersCallback(currentUsers);
+		}
+
+		// Accepts new user object.  Adds id guid and appends to currentUsers array.
+		// Executes createUserCallback with the new user object.
+		function createUser(user, createUserCallback) {
+			// add "id" to new user with a fresh guid
+			user.id = Guid.create().value;
+
+			// add user to currentUsers array
+			currentUsers.push(user);
+
+			// time to execute the callback
+			createUserCallback(user);
+		}
+
+		// Accepts id of user.  Matches to user in currentUsers array.  If found,
+		// user is removed.  Executes deleteUserCallback with currentUsers array.
+		function deleteUserById(id, deleteUserCallback) {
+			// iterate over currentUsers array and look for a match
+			for(var i = 0; i < currentUsers.length; i ++) {
+				if(currentUsers[i].id === id) {
+					// user found! remove it from the array
+					currentUsers.splice(i, 1);
+				}
+				// time to execute the callback
+				deleteUserCallback(currentUsers);
+			}
+		}
+
+		// Accepts id of user and a user object with new properties.  If user is
+		// is found, the user object is updated with the new properties.  Executes
+		// updateUserCallback with the updated user.
+		function updateUser(id, updatedUser, updateUserCallback) {
+			// iterate over currentUsers array and look for a match
+			for(var i = 0; i < currentUsers.length; i++) {
+				if(currentUsers[i].id === id) {
+					// user found!  time to update the user properties
+					for(var attr in updatedUser) {
+						if(updatedUser.hasOwnProperty(attr))
+							currentUsers[i][attr] = updatedUser[attr];
+					}
+					// time to execute the callback
+					updateUserCallback(currentUsers[i]);
+				}
+			}
+		}
+	}
 })();
