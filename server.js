8f10c9b4a5b85336c6f73a437725699a395c511f
Ryan Millay <ryan.p.millay@gmail.com>
2015-11-15 19:20:30 -0500

    polished up new server.js

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.get("/", function(req, res) {
+    res.sendfile('index.html', {root: __dirname });
+});
 
 require("./public/assignment/server/app.js")(app);
 
-app.listen(3000);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+if (typeof ipaddress === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+    ipaddress = "127.0.0.1";
+};
+
+app.listen(port, ipaddress);
