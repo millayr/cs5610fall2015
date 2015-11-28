d2d5ee2b8cedd3ed954d22bba5cff5bbbe120f6b
Ryan Millay <ryan.p.millay@gmail.com>
2015-11-28 12:42:33 -0500

    first pass at field schema

---
+++ public/assignment/server/models/field.schema.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(mongoose) {
+    var FieldSchema = mongoose.Schema({
+        label: String,
+        fieldType: {
+            type: String,
+            enum: ["TEXT", "TEXTAREA", "RADIO", "CHECKBOX", "SELECT", "DATE"]
+        },
+        options: [{
+            label: String,
+            value: String
+        }],
+        placeholder: String
+    }, { collection: "cs5610.assignment.field" });
+    return FieldSchema;
+}
