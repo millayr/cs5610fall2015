c54f3f7cd082331b2183cb937a19be00d2d12c55
Ryan Millay <ryan.p.millay@gmail.com>
2015-12-03 21:01:39 -0500

    fixed back flow bug

--- public/project/client/views/beer/beer-info-sidebar.controller.js
+++ public/project/client/views/beer/beer-info-sidebar.controller.js
@@ -8,16 +8,25 @@
     function BeerInfoSidebarController($scope, $rootScope) {
         // grab the query information that was tucked away in the root scope.
         // we'll use this to build the "back" link.
-        $scope.backPageUrl =
-            ($rootScope.backPageUrl == undefined ? "#/home" : $rootScope.backPageUrl);
+        if($rootScope.backPageUrl != undefined) {
+            $scope.backPageUrl = $rootScope.backPageUrl;
+            delete $rootScope.backPageUrl;
+        } else {
+            $scope.backPageUrl = "#/home";
+        }
 
         if($rootScope.backData != undefined) {
             $scope.backData = $rootScope.backData;
+            delete $rootScope.backData;
         }
 
         // populate the img url once we know what beer we're displaying
         $scope.$on("beerLoad", function(event, beer) {
-            $scope.imgUrl = beer.labels.medium;
+            if(beer.labels == undefined) {
+                $scope.imgUrl = "./img/generic_beer.png";
+            } else {
+                $scope.imgUrl = beer.labels.medium;
+            }
         });
     }
 })();
--- public/project/client/views/brewery/brewery-info-sidebar.controller.js
+++ public/project/client/views/brewery/brewery-info-sidebar.controller.js
@@ -5,9 +5,28 @@
         .module("BrewHouseApp")
         .controller("BreweryInfoSidebarController", BreweryInfoSidebarController);
 
-    function BreweryInfoSidebarController($scope) {
+    function BreweryInfoSidebarController($scope, $rootScope) {
+        // grab the query information that was tucked away in the root scope.
+        // we'll use this to build the "back" link.
+        if($rootScope.backPageUrl != undefined) {
+            $scope.backPageUrl = $rootScope.backPageUrl;
+            delete $rootScope.backPageUrl;
+        } else {
+            $scope.backPageUrl = "#/home";
+        }
+
+        if($rootScope.backData != undefined) {
+            $scope.backData = $rootScope.backData;
+            delete $rootScope.backData;
+        }
+
         $scope.$on("breweryLoad", function(event, brewery) {
-            $scope.imgUrl = brewery.images.medium;
+            $scope.breweryid = brewery.id;
+            if(brewery.images == undefined) {
+                $scope.imgUrl = "./img/generic_brewery.jpg";
+            } else {
+                $scope.imgUrl = brewery.images.medium;
+            }
         });
     }
 })();
