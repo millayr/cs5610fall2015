366bc55465a628ec32ba3005db63a5962466dcee
Ryan Millay <ryan.p.millay@gmail.com>
2015-12-06 09:22:38 -0500

    fixed bug with the back logic

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -5,11 +5,15 @@
         .module("BrewHouseApp")
         .controller("HomeController", HomeController);
 
-    function HomeController(BeerService, BreweryService) {
+    function HomeController($rootScope, BeerService, BreweryService) {
         var model = this;
         var numTrending = 6;
         model.beers = [];
         model.breweries = [];
+
+        if($rootScope.backPageUrl != undefined) {
+            delete $rootScope.backPageUrl;
+        }
 
         BeerService.getTrendingBeers(numTrending)
             .then(function (trendingBeers) {
